2bb077501348c845629684fe9550c7d1c922c738
Patrick Jones <forksofpower@gmail.com>
2023-02-14 17:59:27 -0500

    complete rust/lucians-lucious-lasagna

---
+++ exercism/rust/lucians-luscious-lasagna/src/lib.rs
@@ -0,0 +1,18 @@
+const MIN_IN_OVEN: i32 = 40;
+const MIN_PER_LAYER: i32 = 2;
+
+pub fn expected_minutes_in_oven() -> i32 {
+    MIN_IN_OVEN
+}
+
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+    expected_minutes_in_oven() - actual_minutes_in_oven
+}
+
+pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+    number_of_layers * MIN_PER_LAYER
+}
+
+pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+}
