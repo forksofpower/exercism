9ad65c6da03f6ab10774faf74f30844b9c1fec63
Patrick Jones <forksofpower@gmail.com>
2023-02-15 18:00:25 -0500

    complete rust/assembly-line

---
+++ exercism/rust/assembly-line/src/lib.rs
@@ -0,0 +1,16 @@
+const MIN_RATE: f64 = 221.0; // cars per hour
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    MIN_RATE
+        * (speed as f64)
+        * match speed {
+            0 => 0.0,
+            1..=4 => 1.0,
+            5..=8 => 0.9,
+            9..=u8::MAX => 0.77,
+        }
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
