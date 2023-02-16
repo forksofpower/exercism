d30a10bb1f0065b0ef92cb1222abdf253350423f
Patrick Jones <forksofpower@gmail.com>
2023-02-16 18:01:06 -0500

    complete rust/semi-structured-logs

---
+++ exercism/rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,29 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::fmt::format;
+
+/// various log levels
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+    Debug,
+}
+
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    let l = format!("{:?}", level);
+    format!("[{}]: {}", l.to_uppercase(), message)
+}
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+pub fn warn(message: &str) -> String {
+    log(LogLevel::Warning, message)
+}
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
